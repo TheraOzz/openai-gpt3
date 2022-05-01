cbc05dde04bb55db750c90bf715a102d74961ff0
rich-ter <me@philipposrichter.com>
2022-05-02 02:40:26 +0300

    reproducing the gpt-3 answer in a voice format

--- gpt3_django/main_app/gpt3_models.py
+++ gpt3_django/main_app/gpt3_models.py
@@ -2,7 +2,7 @@ import os
 import openai
 import config_api_keys
 openai.api_key = config_api_keys.OPENAI_API_KEY
-
+from gtts import gTTS
 #this is a Q&A machine learning algorithm using OPEN AI 
 def gpt3_question_and_answer(query):
     response = openai.Completion.create(
@@ -53,4 +53,11 @@ def gpt3_chat_with_ai(query):
 # print(gpt3_question_and_answer(query))
 
 query = 'why am I sad?'
-print(gpt3_chat_with_ai(query))
+gpt3_text = gpt3_chat_with_ai(query)
+
+
+#reproducing the GPT-3 ANSWER IN A VOICE FORMAT
+language = 'en'
+myobj = gTTS(text=gpt3_text, lang=language, slow=False)
+myobj.save("trial.mp3")
+os.system("trial.mp3")
--- gpt3_django/main_app/views.py
+++ gpt3_django/main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from . import gpt3_models
+
+# gpt3_models.gpt3_question_and_answer()
+
